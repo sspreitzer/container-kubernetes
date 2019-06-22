40731298d2b59a80dcf355a445789956a580ecb2
Sascha Spreitzer <sascha@redhat.com>
2019-06-22 18:49:51 +0200

    Version 1.15.0

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM centos:7
 
-ARG VERSION=v1.13.4
+ARG VERSION=1.15.0
 
+ADD kubernetes.repo /etc/yum.repos.d/kubernetes.repo
 ADD build.sh /build.sh
 RUN /build.sh
--- build.sh
+++ build.sh
@@ -3,17 +3,14 @@
 set -x
 set -e
 
-yum install -y iptables ebtables socat
+yum install -y kubeadm-${VERSION} kubelet-${VERSION} kubectl-${VERSION}
 rm -rf /var/cache/yum
 
 cd /tmp
-curl -L "https://dl.k8s.io/${VERSION}/kubernetes-server-linux-amd64.tar.gz" > kubernetes.tar.gz
+curl -L "https://dl.k8s.io/v${VERSION}/kubernetes-server-linux-amd64.tar.gz" > kubernetes.tar.gz
 tar xvzf kubernetes.tar.gz
 install -v -o root -g root -m 0755 kubernetes/server/bin/kube-apiserver /usr/bin/kube-apiserver
 install -v -o root -g root -m 0755 kubernetes/server/bin/kube-controller-manager /usr/bin/kube-controller-manager
 install -v -o root -g root -m 0755 kubernetes/server/bin/kube-scheduler /usr/bin/kube-scheduler
 install -v -o root -g root -m 0755 kubernetes/server/bin/kube-proxy /usr/bin/kube-proxy
-install -v -o root -g root -m 0755 kubernetes/server/bin/kubelet /usr/bin/kubelet
-install -v -o root -g root -m 0755 kubernetes/server/bin/kubectl /usr/bin/kubectl
-install -v -o root -g root -m 0755 kubernetes/server/bin/kubeadm /usr/bin/kubeadm
 rm -rf kubernetes*
